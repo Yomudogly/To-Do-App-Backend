db0e0b31d7feac3a3c60547806a194c949230153
Roman Khalnepesov <khalnepesov@gmail.com>
2020-01-15 15:27:30 +0000

    SQLAlchemy model changed

--- src/models.py
+++ src/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(250), unique=False, nullable=False)
-    done = db.Column(db.Boolean, nullable=False)
+    done = db.Column(db.Boolean, unique=False, default=False)
 
     def __repr__(self):
         return '<Todo %r>' % self.text
